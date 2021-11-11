d10d071146cb0ede50a04a6f95eed07cabdd587d
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-11-11 12:30:36 +0000

    user login/create/logout/authenticate/session

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :about, only: [:index]
-  
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
